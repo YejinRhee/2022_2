9426cd540dfc9545f79ee702614afb0641f4cfe2
YejinRhee <yjlee9828@gmail.com>
2022-06-28 11:29:49 +0900

    적합한 자료구조 이용 -> Big O 줄이자

---
+++ AlgorithmCamp/BJ2162_Deque_BIGO.py
@@ -0,0 +1,10 @@
+from collections import deque
+n = int(input())
+de = deque([]) # de = deque(range(1,n+1))
+for i in range(n):
+    de.append(i+1)
+while len(de) > 1: # 매 수행마다 len은 1씩 줄어
+    de.popleft()
+    de.append(de.popleft())
+
+print(de)
