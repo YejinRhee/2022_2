cd5f8cabbb0764ad7e9fd1422f504368b9001be6
YejinRhee <yjlee9828@gmail.com>
2022-06-28 15:37:38 +0900

    refactor : first setting

--- AlgorithmCamp/BJ2162_Deque_BIGO.py
+++ AlgorithmCamp/BJ2162_Deque_BIGO.py
@@ -1,9 +1,9 @@
-# from collections import deque
-# n = int(input())
-# de = deque([]) # de = deque(range(1,n+1))
-# for i in range(n):
-#     de.append(i+1)
-# while len(de) > 1: # 매 수행마다 len은 1씩 줄어
-#     de.popleft()
-#     de.append(de.popleft())
-# print(de)
+from collections import deque
+n = int(input())
+de = deque([]) # de = deque(range(1,n+1))
+for i in range(n):
+    de.append(i+1)
+while len(de) > 1: # 매 수행마다 len은 1씩 줄어
+    de.popleft()
+    de.append(de.popleft())
+print(de)
