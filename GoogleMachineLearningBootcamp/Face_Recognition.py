befab18e069ba0d07859f53797ce8d905d3f2a43
Yejin <yjlee9828@gmail.com>
2022-08-10 21:44:26 +0900

    Create Face_Recognition.py
    1 - Packages
    2 - Naive Face Verification
    3 - Encoding Face Images into a 128-Dimensional Vector
    3.1 - Using a ConvNet to Compute Encodings
    3.2 - The Triplet Loss
    4 - Loading the Pre-trained Model
    5 - Applying the Model
    5.1 - Face Verification
    5.2 - Face Recognition
    6 - References

---
+++ GoogleMachineLearningBootcamp/Face_Recognition.py
@@ -0,0 +1,122 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import MaxPooling2D, AveragePooling2D
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Lambda, Flatten, Dense
+from tensorflow.keras.initializers import glorot_uniform
+from tensorflow.keras.layers import Layer
+from tensorflow.keras import backend as K
+K.set_image_data_format('channels_last')
+import os
+import numpy as np
+from numpy import genfromtxt
+import pandas as pd
+import tensorflow as tf
+import PIL
+
+%matplotlib inline
+%load_ext autoreload
+%autoreload 2
+
+
+from tensorflow.keras.models import model_from_json
+
+json_file = open('keras-facenet-h5/model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights('keras-facenet-h5/model.h5')
+
+
+# UNQ_C1(UNIQUE CELL IDENTIFIER, DO NOT EDIT)
+# GRADED FUNCTION: triplet_loss
+
+def triplet_loss(y_true, y_pred, alpha = 0.2):
+    """
+    Implementation of the triplet loss as defined by formula (3)
+    
+    Arguments:
+    y_true -- true labels, required when you define a loss in Keras, you don't need it in this function.
+    y_pred -- python list containing three objects:
+            anchor -- the encodings for the anchor images, of shape (None, 128)
+            positive -- the encodings for the positive images, of shape (None, 128)
+            negative -- the encodings for the negative images, of shape (None, 128)
+    
+    Returns:
+    loss -- real number, value of the loss
+    """
+    
+    anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
+    
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,positive)),axis=-1)
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)),axis=-1)
+    basic_loss = tf.add(tf.subtract(pos_dist,neg_dist),alpha)
+    loss = tf.reduce_sum(tf.maximum(basic_loss,0.0))
+    
+    return loss
+  
+  
+def img_to_encoding(image_path, model):
+    img = tf.keras.preprocessing.image.load_img(image_path, target_size=(160, 160))
+    img = np.around(np.array(img) / 255.0, decimals=12)
+    x_train = np.expand_dims(img, axis=0)
+    embedding = model.predict_on_batch(x_train)
+    return embedding / np.linalg.norm(embedding, ord=2)
+
+
+def verify(image_path, identity, database, model):
+    """
+    Function that verifies if the person on the "image_path" image is "identity".
+    
+    Arguments:
+        image_path -- path to an image
+        identity -- string, name of the person you'd like to verify the identity. Has to be an employee who works in the office.
+        database -- python dictionary mapping names of allowed people's names (strings) to their encodings (vectors).
+        model -- your Inception model instance in Keras
+    
+    Returns:
+        dist -- distance between the image_path and the image of "identity" in the database.
+        door_open -- True, if the door should open. False otherwise.
+    """
+    encoding = img_to_encoding(image_path, model)
+    dist = np.linalg.norm(encoding - database[identity])
+    if dist<0.7:
+        print("It's " + str(identity) + ", welcome in!")
+        door_open = True
+    else:
+        print("It's not " + str(identity) + ", please go away")
+        door_open = False 
+    return dist, door_open
+
+
+def who_is_it(image_path, database, model):
+    """
+    Implements face recognition for the office by finding who is the person on the image_path image.
+    
+    Arguments:
+        image_path -- path to an image
+        database -- database containing image encodings along with the name of the person on the image
+        model -- your Inception model instance in Keras
+    
+    Returns:
+        min_dist -- the minimum distance between image_path encoding and the encodings from the database
+        identity -- string, the name prediction for the person on image_path
+    """
+    
+    encoding = img_to_encoding(image_path, model)
+    min_dist = 100
+    
+    for (name, db_enc) in database.items():
+        dist = np.linalg.norm(encoding - db_enc)
+        if  dist < min_dist:
+            min_dist = dist
+            identity = name
+    
+    if min_dist > 0.7:
+        print("Not in the database.")
+    else:
+        print ("it's " + str(identity) + ", the distance is " + str(min_dist))
+        
+    return min_dist, identity
