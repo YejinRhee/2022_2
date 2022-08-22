ee97a41c2eb0075a7c52425525d8d4efcb6b8e5f
Yejin <yjlee9828@gmail.com>
2022-08-23 01:02:48 +0900

    Create 20220821_Transformers_Architectures_with_Tensorflow_3.py

---
+++ GoogleMachineLearningBootcamp/20220821_Transformers_Architectures_with_Tensorflow_3.py
@@ -0,0 +1,111 @@
+import tensorflow as tf
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    The entire Encoder starts by passing the input to an embedding layer 
+    and using positional encoding to then pass the output through a stack of
+    encoder Layers
+        
+    """  
+    def __init__(self, num_layers, embedding_dim, num_heads, fully_connected_dim, input_vocab_size,
+               maximum_position_encoding, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(Encoder, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(input_vocab_size, self.embedding_dim)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, 
+                                                self.embedding_dim)
+
+
+        self.enc_layers = [EncoderLayer(embedding_dim=self.embedding_dim,
+                                        num_heads=num_heads,
+                                        fully_connected_dim=fully_connected_dim,
+                                        dropout_rate=dropout_rate,
+                                        layernorm_eps=layernorm_eps) 
+                           for _ in range(self.num_layers)]
+
+        self.dropout = Dropout(dropout_rate)
+        
+    def call(self, x, training, mask):
+        """
+        Forward pass for the Encoder
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, input_seq_len)
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            mask -- Boolean mask to ensure that the padding is not 
+                    treated as part of the input
+        Returns:
+            out2 -- Tensor of shape (batch_size, input_seq_len, fully_connected_dim)
+        """
+        seq_len = tf.shape(x)[1]
+        x = self.embedding(x)  # (batch_size, input_seq_len, embedding_dim)
+        x *= tf.math.sqrt(tf.cast(self.embedding_dim, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training = training)
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+
+        return x  # (batch_size, input_seq_len, fully_connected_dim)
+
+
+class DecoderLayer(tf.keras.layers.Layer):
+    """
+    The decoder layer is composed by two multi-head attention blocks, 
+    one that takes the new input and uses self-attention, and the other 
+    one that combines it with the output of the encoder, followed by a
+    fully connected block. 
+    """
+    def __init__(self, embedding_dim, num_heads, fully_connected_dim, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(DecoderLayer, self).__init__()
+
+        self.mha1 = MultiHeadAttention(num_heads=num_heads,
+                                      key_dim=embedding_dim,
+                                      dropout=dropout_rate)
+
+        self.mha2 = MultiHeadAttention(num_heads=num_heads,
+                                      key_dim=embedding_dim,
+                                      dropout=dropout_rate)
+
+        self.ffn = FullyConnected(embedding_dim=embedding_dim,
+                                  fully_connected_dim=fully_connected_dim)
+
+        self.layernorm1 = LayerNormalization(epsilon=layernorm_eps)
+        self.layernorm2 = LayerNormalization(epsilon=layernorm_eps)
+        self.layernorm3 = LayerNormalization(epsilon=layernorm_eps)
+
+        self.dropout_ffn = Dropout(dropout_rate)
+    
+    def call(self, x, enc_output, training, look_ahead_mask, padding_mask):
+        """
+        Forward pass for the Decoder Layer
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+            enc_output --  Tensor of shape(batch_size, input_seq_len, fully_connected_dim)
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            look_ahead_mask -- Boolean mask for the target_input
+            padding_mask -- Boolean mask for the second multihead attention layer
+        Returns:
+            out3 -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+            attn_weights_block1 -- Tensor of shape(batch_size, num_heads, target_seq_len, input_seq_len)
+            attn_weights_block2 -- Tensor of shape(batch_size, num_heads, target_seq_len, input_seq_len)
+        """
+        mult_attn_out1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask, return_attention_scores=True, training=training)  # (batch_size, target_seq_len, d_model)
+        Q1 = self.layernorm1(mult_attn_out1 + x)
+        mult_attn_out2, attn_weights_block2 = self.mha2(Q1, enc_output, enc_output, 
+                                                        padding_mask, return_attention_scores=True, training=training)  # (batch_size, target_seq_len, d_model)
+        
+        mult_attn_out2 = self.layernorm2(mult_attn_out2 + Q1)  # (batch_size, target_seq_len, fully_connected_dim)
+        ffn_output = self.ffn(mult_attn_out2)  # (batch_size, target_seq_len, fully_connected_dim)
+        ffn_output = self.dropout_ffn(ffn_output, training=training)
+        out3 =  self.layernorm3(ffn_output + mult_attn_out2)  # (batch_size, target_seq_len, fully_connected_dim)
+        return out3, attn_weights_block1, attn_weights_block2
