90da3cb3033fdeaf599452d70e9eb1823865913b
Yejin <yjlee9828@gmail.com>
2022-08-25 16:15:48 +0900

    Create 20220825_Transformers_Architectures_with_Tensorflow_4.py

---
+++ GoogleMachineLearningBootcamp/20220825_Transformers_Architectures_with_Tensorflow_4.py
@@ -0,0 +1,118 @@
+import tensorflow as tf
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Decoder(tf.keras.layers.Layer):
+    """
+    The entire Encoder is starts by passing the target input to an embedding layer 
+    and using positional encoding to then pass the output through a stack of
+    decoder Layers
+        
+    """ 
+    def __init__(self, num_layers, embedding_dim, num_heads, fully_connected_dim, target_vocab_size,
+               maximum_position_encoding, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(Decoder, self).__init__()
+
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.embedding = Embedding(target_vocab_size, self.embedding_dim)
+        self.pos_encoding = positional_encoding(maximum_position_encoding, self.embedding_dim)
+
+        self.dec_layers = [DecoderLayer(embedding_dim=self.embedding_dim,
+                                        num_heads=num_heads,
+                                        fully_connected_dim=fully_connected_dim,
+                                        dropout_rate=dropout_rate,
+                                        layernorm_eps=layernorm_eps) 
+                           for _ in range(self.num_layers)]
+        self.dropout = Dropout(dropout_rate)
+    
+    def call(self, x, enc_output, training, 
+           look_ahead_mask, padding_mask):
+        """
+        Forward  pass for the Decoder
+        
+        Arguments:
+            x -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+            enc_output --  Tensor of shape(batch_size, input_seq_len, fully_connected_dim)
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            look_ahead_mask -- Boolean mask for the target_input
+            padding_mask -- Boolean mask for the second multihead attention layer
+        Returns:
+            x -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+            attention_weights - Dictionary of tensors containing all the attention weights
+                                each of shape Tensor of shape (batch_size, num_heads, target_seq_len, input_seq_len)
+        """
+
+        seq_len = tf.shape(x)[1]
+        attention_weights = {}
+        x = self.embedding(x)  # (batch_size, target_seq_len, fully_connected_dim)
+        x *= tf.sqrt(tf.cast(self.embedding_dim, tf.float32))
+        x += self.pos_encoding[:, :seq_len, :]
+        x = self.dropout(x, training=training)
+        for i in range(self.num_layers):
+            x, block1, block2 = self.dec_layers[i](x, enc_output, training, look_ahead_mask, padding_mask)
+            attention_weights['decoder_layer{}_block1_self_att'.format(i+1)] = block1
+            attention_weights['decoder_layer{}_block2_decenc_att'.format(i+1)] = block2
+        return x, attention_weights
+
+
+Decoder_test(Decoder, create_look_ahead_mask, create_padding_mask)
+
+
+class Transformer(tf.keras.Model):
+    """
+    Complete transformer with an Encoder and a Decoder
+    """
+    def __init__(self, num_layers, embedding_dim, num_heads, fully_connected_dim, input_vocab_size, 
+               target_vocab_size, max_positional_encoding_input,
+               max_positional_encoding_target, dropout_rate=0.1, layernorm_eps=1e-6):
+        super(Transformer, self).__init__()
+
+        self.encoder = Encoder(num_layers=num_layers,
+                               embedding_dim=embedding_dim,
+                               num_heads=num_heads,
+                               fully_connected_dim=fully_connected_dim,
+                               input_vocab_size=input_vocab_size,
+                               maximum_position_encoding=max_positional_encoding_input,
+                               dropout_rate=dropout_rate,
+                               layernorm_eps=layernorm_eps)
+
+        self.decoder = Decoder(num_layers=num_layers, 
+                               embedding_dim=embedding_dim,
+                               num_heads=num_heads,
+                               fully_connected_dim=fully_connected_dim,
+                               target_vocab_size=target_vocab_size, 
+                               maximum_position_encoding=max_positional_encoding_target,
+                               dropout_rate=dropout_rate,
+                               layernorm_eps=layernorm_eps)
+
+        self.final_layer = Dense(target_vocab_size, activation='softmax')
+    
+    def call(self, input_sentence, output_sentence, training, enc_padding_mask, look_ahead_mask, dec_padding_mask):
+        """
+        Forward pass for the entire Transformer
+        Arguments:
+            input_sentence -- Tensor of shape (batch_size, input_seq_len, fully_connected_dim)
+                              An array of the indexes of the words in the input sentence
+            output_sentence -- Tensor of shape (batch_size, target_seq_len, fully_connected_dim)
+                              An array of the indexes of the words in the output sentence
+            training -- Boolean, set to true to activate
+                        the training mode for dropout layers
+            enc_padding_mask -- Boolean mask to ensure that the padding is not 
+                    treated as part of the input
+            look_ahead_mask -- Boolean mask for the target_input
+            dec_padding_mask -- Boolean mask for the second multihead attention layer
+        Returns:
+            final_output -- Describe me
+            attention_weights - Dictionary of tensors containing all the attention weights for the decoder
+                                each of shape Tensor of shape (batch_size, num_heads, target_seq_len, input_seq_len)
+        
+        """
+        enc_output = self.encoder(input_sentence, training, enc_padding_mask)  # (batch_size, inp_seq_len, fully_connected_dim)
+        dec_output, attention_weights = self.decoder(output_sentence, enc_output, training, look_ahead_mask, dec_padding_mask)
+        final_output = self.final_layer(dec_output) # (batch_size, tar_seq_len, target_vocab_size)
+
+        return final_output, attention_weights
