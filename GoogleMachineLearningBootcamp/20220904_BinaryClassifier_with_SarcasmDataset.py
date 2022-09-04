ec55d23ed38a4547486d20460386164316b5db10
Yejin Rhee <yjlee9828@gmail.com>
2022-09-04 12:52:07 +0900

    _TFcourse3_W2

--- GoogleMachineLearningBootcamp/20220904_BinaryClassifier_with_SarcasmDataset.py
+++ GoogleMachineLearningBootcamp/20220904_BinaryClassifier_with_SarcasmDataset.py
@@ -1,36 +1,95 @@
+import json
+
+with open("./sarcasm.json", 'r') as f:
+    datastore = json.load(f)
+
+sentences = []
+labels = []
+for item in datastore:
+    sentences.append(item['headline'])
+    labels.append(item['is_sarcastic'])
+
+training_size = 20000
+vocab_size = 10000
+max_length = 32
+embedding_dim = 16
+
+
+training_sentences = sentences[0:training_size]
+testing_sentences = sentences[training_size:]
+training_labels = labels[0:training_size]
+testing_labels = labels[training_size:]
+
+
+import numpy as np
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-sentences = [
-    'I love my dog',
-    'I love my cat',
-    'You love my dog!',
-    'Do you think my dog is amazing?'
-]
-
-tokenizer = Tokenizer(num_words=100, oov_token="<OOV>")
-
-tokenizer.fit_on_texts(sentences)
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(training_sentences)
 word_index = tokenizer.word_index
-sequences = tokenizer.texts_to_sequences(sentences)
+training_sequences = tokenizer.texts_to_sequences(training_sentences)
+training_padded = pad_sequences(training_sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+testing_sequences = tokenizer.texts_to_sequences(testing_sentences)
+testing_padded = pad_sequences(testing_sequences)
+training_labels = np.array(training_labels)
+testing_labels = np.array(testing_labels)
 
-print("\nWord Index = ", word_index)
-print("\nSequences = ", sequences)
 
+import tensorflow as tf
+gap1d_layer = tf.keras.layers.GlobalAveragePooling1D()
+sample_array = np.array([[[10,2],[1,3],[1,1]]])
+print(f'shape of sample_array = {sample_array.shape}')
+print(f'sample array: {sample_array}')
+output = gap1d_layer(sample_array)
+print(f'output shape of gap1d_layer: {output.shape}')
+print(f'output array of gap1d_layer: {output.numpy()}')
 
-padded = pad_sequences(sequences, maxlen=5)
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
 
-print("\nPadded Sequences:")
-print(padded)
+model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
 
-test_data = [
-    'i really love my dog',
-    'my dog loves my manatee'
-]
+import matplotlib.pyplot as plt
 
-test_seq = tokenizer.texts_to_sequences(test_data)
-print("\nWord Index = ", word_index)
-print("\nTest Sequence = ", test_seq)
-padded = pad_sequences(test_seq, maxlen=10)
-print("\nPadded Test Sequence: ")
-print(padded)
+def plot_graphs(history, string):
+  plt.plot(history.history[string])
+  plt.plot(history.history['val_'+string])
+  plt.xlabel("Epochs")
+  plt.ylabel(string)
+  plt.legend([string, 'val_'+string])
+  plt.show()
+plot_graphs(history, "accuracy")
+plot_graphs(history, "loss")
+
+reverse_word_index = tokenizer.index_word
+embedding_layer = model.layers[0]
+embedding_weights = embedding_layer.get_weights()[0]
+print(embedding_weights.shape) 
+
+import io
+out_v = io.open('vecs.tsv', 'w', encoding='utf-8')
+out_m = io.open('meta.tsv', 'w', encoding='utf-8')
+for word_num in range(1, vocab_size):
+  word_name = reverse_word_index[word_num]
+  word_embedding = embedding_weights[word_num]
+  out_m.write(word_name + "\n")
+  out_v.write('\t'.join([str(x) for x in word_embedding]) + "\n")
+
+out_v.close()
+out_m.close()
+
+try:
+  from google.colab import files
+except ImportError:
+  pass
+else:
+  files.download('vecs.tsv')
+  files.download('meta.tsv')
